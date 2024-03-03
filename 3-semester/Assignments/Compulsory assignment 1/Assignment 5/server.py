01d838d9e76273934094d3024b449b77decf27d6
GKierk <45508528+GKierk@users.noreply.github.com>
2024-03-03 13:11:58 +0100

    Implemented server which responds to random

---
+++ 3-semester/Assignments/Compulsory assignment 1/Assignment 5/server.py
@@ -0,0 +1,54 @@
+import json
+from socket import *
+from random import randint
+from threading import Thread
+
+serverPort = 5000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('Server is ready to listen')
+
+def random_number(connectionSocket, data):
+    try:
+        number1, number2 = data["numbers"].split()
+        number1 = int(number1)
+        number2 = int(number2)
+        if number1 > number2:
+            connectionSocket.send(json.dumps({"response": randint(number2, number1)}).encode())
+            return
+        connectionSocket.send(json.dumps({"response": randint(number1, number2)}).encode())
+        return
+    except ValueError:
+        connectionSocket.send(json.dumps({"error": "Invalid input"}).encode())
+        return
+
+def handle_protocol(connectionSocket):
+    operation = connectionSocket.recv(1024).decode()
+    operation = json.loads(operation)
+    connectionSocket.send(json.dumps({"response": "Input numbers"}).encode())
+    data = connectionSocket.recv(1024).decode()
+    data = json.loads(data)
+    match operation["operation"]:
+        case "random":
+            random_number(connectionSocket, data)
+        case _:
+            connectionSocket.send(json.dumps({"error": "Invalid operation"}).encode())
+
+def handle_client_request(connectionSocket, addr):
+    print(addr[0])
+    handle_protocol(connectionSocket)
+    connectionSocket.close()
+
+def start_server():
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        t = Thread(target=handle_client_request, args=(connectionSocket, addr))
+        t.start()
+
+
+def main():
+    start_server()
+
+if __name__ == "__main__":
+    main()
