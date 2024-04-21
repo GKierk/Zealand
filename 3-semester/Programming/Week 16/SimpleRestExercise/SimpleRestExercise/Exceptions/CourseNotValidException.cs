a732fe7666d66ba0044abf42ffce2e1c5a39061b
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 09:47:52 +0200

    Implemented CourseNotValidException

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Exceptions/CourseNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleRestExercise.Exceptions;
+
+public class CourseNotValidException: Exception
+{
+    public CourseNotValidException() { }
+
+    public CourseNotValidException(string message) : base(message) { }
+
+    public CourseNotValidException(string message, Exception innerException) : base(message, innerException) { }
+}
