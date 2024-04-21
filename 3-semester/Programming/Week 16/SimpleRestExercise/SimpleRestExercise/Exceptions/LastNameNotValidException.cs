971b32b47010c28456c39c89d3ed469dd9b7a789
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 08:49:35 +0200

    Implemented LastNameNotValidException

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Exceptions/LastNameNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleRestExercise.Exceptions;
+
+public class LastNameNotValidException: Exception
+{
+    public LastNameNotValidException() { }
+
+    public LastNameNotValidException(string message) : base(message) { }
+
+    public LastNameNotValidException(string message, Exception innerException) : base(message, innerException) { }
+}
