2c4728e5d5ee94dd7d0da53a94682a7d684850d1
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 08:48:36 +0200

    Implemented IdNotValidException

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Exceptions/IdNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleRestExercise.Exceptions;
+
+public class IdNotValidException: Exception
+{
+    public IdNotValidException() { }
+
+    public IdNotValidException(string message) : base(message) { }
+
+    public IdNotValidException(string message,  Exception innerException) : base(message, innerException) { }
+}
