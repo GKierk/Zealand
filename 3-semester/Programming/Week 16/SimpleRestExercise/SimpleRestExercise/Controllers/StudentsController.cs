f8cdc2f70babc468b4fd956cebaf889bb934467f
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 12:25:02 +0200

    Added StudentsController

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleRestExercise.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleRestExercise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            foreach (var student in StudentsRepository.Instance.Read())
+            {
+                Console.WriteLine(student);
+            }
+
+            return StudentsRepository.Instance.Read();
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return StudentsRepository.Instance.Read(id);
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public async Task Post([FromBody] Student student)
+        {
+            await StudentsRepository.Instance.CreateAsync(student);
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
