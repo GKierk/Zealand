12b4e5a8a7b78b4f67df0723fba2654a43b696d4
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 09:58:30 +0200

    Added method to enroll in courses and method for verifying

--- 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Student.cs
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Student.cs
@@ -9,6 +9,10 @@ public class Student
     public string? LastName { get; set; }
     public string FullName => $"{FirstName} {LastName}";
 
+    private List<Course> enrolledCourses = new List<Course>();
+
+    public List<Course>? EnrolledCourses => enrolledCourses;
+
     public override string ToString()
     {
         return FullName;
@@ -40,10 +44,33 @@ public class Student
         }
     }
 
+    public void VerifyEnrolledCourses()
+    {
+        List<Course> existingCourses = CoursesRepository.Instance.Courses;
+
+        foreach (Course course in existingCourses)
+        {
+            if (EnrolledCourses!.ToString() != course.Name)
+            {
+                throw new CourseNotValidException("One of the selected courses is not available as one of the courses offered.");
+            }
+        }
+    }
+
     public void Verify()
     {
         VerifyId();
         VerifyFirstName();
         VerifyLastName();
+        VerifyEnrolledCourses();
+    }
+
+    public Course EnrollCourse(string? course, CoursesRepository avaialabeCourses)
+    {
+        Course courseToEnroll = avaialabeCourses.Courses.FirstOrDefault(c => c.Name == course)!;
+        EnrolledCourses!.Add(courseToEnroll);
+        VerifyEnrolledCourses();
+
+        return courseToEnroll;
     }
 }
