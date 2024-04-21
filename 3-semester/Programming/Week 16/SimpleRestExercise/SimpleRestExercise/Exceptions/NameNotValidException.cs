4235123fa547aed2bb7b51bfe090ca0cd85c7529
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 08:58:08 +0200

    Implemented NameNotValidException

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Exceptions/NameNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleRestExercise.Exceptions;
+
+public class NameNotValidException: Exception
+{
+    public NameNotValidException() { }
+
+    public NameNotValidException(string message) : base(message) { }
+
+    public NameNotValidException(string message, Exception innerException) : base(message, innerException) { }
+}
--- 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Course.cs
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Course.cs
@@ -1,7 +1,25 @@
-﻿namespace SimpleRestExercise.Models;
+﻿using SimpleRestExercise.Exceptions;
+
+namespace SimpleRestExercise.Models;
 
 public class Course
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+
+    public void VerifyId()
+    {
+        if (Id < 0)
+        {
+            throw new IdNotValidException("The id can't be negative.");
+        }
+    }
+
+    public void VerifyName()
+    {
+        if (!string.IsNullOrEmpty(Name))
+        {
+            throw new NameNotValidException("The course name can't be empty");
+        }
+    }
 }
