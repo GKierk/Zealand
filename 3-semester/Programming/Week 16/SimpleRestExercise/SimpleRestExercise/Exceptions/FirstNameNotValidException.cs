301ab5b5f2b0d0e65a3c4cd32355dff987a2538d
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 08:49:20 +0200

    Implemented FirstNameNotValidException

---
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Exceptions/FirstNameNotValidException.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleRestExercise.Exceptions;
+
+public class FirstNameNotValidException: Exception
+{
+    public FirstNameNotValidException() { }
+
+    public FirstNameNotValidException(string message) : base(message) { }
+
+    public FirstNameNotValidException(string message, Exception innerException) : base(message, innerException) { }
+}
