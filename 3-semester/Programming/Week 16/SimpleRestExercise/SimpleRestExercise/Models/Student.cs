761c92e883f436361546989048f989bd10e989c0
GKierk <45508528+GKierk@users.noreply.github.com>
2024-04-21 08:58:29 +0200

    Added methods to verify properties

--- 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Student.cs
+++ 3-semester/Programming/Week 16/SimpleRestExercise/SimpleRestExercise/Models/Student.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleRestExercise.Models;
+﻿using SimpleRestExercise.Exceptions;
+
+namespace SimpleRestExercise.Models;
 
 public class Student
 {
@@ -6,4 +8,37 @@ public class Student
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string FullName => $"{FirstName} {LastName}";
+
+    public int VerifyId()
+    {
+        if (Id < 0)
+        {
+            throw new IdNotValidException("The id can't be negative.");
+        }
+
+        return Id;
+    }
+
+    public void VerifyFirstName()
+    {
+        if (string.IsNullOrEmpty(FirstName))
+        {
+            throw new FirstNameNotValidException("You must enter a first name.");
+        }
+    }
+
+    public void VerifyLastName()
+    {
+        if (!string.IsNullOrEmpty(LastName))
+        {
+            throw new LastNameNotValidException("You must enter a last name.");
+        }
+    }
+
+    public void Verify()
+    {
+        VerifyId();
+        VerifyFirstName();
+        VerifyLastName();
+    }
 }
